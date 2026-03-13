c0e7943d9a91517cd2a4593749ec46840d50e03b
Vignesh Natarajkumar <vignesh.n004@gmail.com>
2026-03-13 14:07:09 +0530

    Initial commit with assignment 1 PS

---
+++ Assignment1/NLP Assignemnet/Cranfield Dataset NLP/template_code_part1/tokenization.py
@@ -0,0 +1,74 @@
+from util import *
+
+# Add your import statements here
+# (Students may import required libraries such as nltk, spacy, re, etc.)
+
+
+class Tokenization():
+
+	def naive(self, text):
+		"""
+		Tokenization using a Naive Approach
+
+		Parameters
+		----------
+		arg1 : list
+			A list of strings where each string is a single sentence
+
+		Returns
+		-------
+		list
+			A list of lists where each sub-list is a sequence of tokens
+		"""
+
+		tokenizedText = None
+
+		# Fill in code here
+
+		return tokenizedText
+
+
+
+	def pennTreeBank(self, text):
+		"""
+		Tokenization using the Penn Tree Bank Tokenizer
+
+		Parameters
+		----------
+		arg1 : list
+			A list of strings where each string is a single sentence
+
+		Returns
+		-------
+		list
+			A list of lists where each sub-list is a sequence of tokens
+		"""
+
+		tokenizedText = None
+
+		# Fill in code here
+
+		return tokenizedText
+
+
+
+	def spacyTokenizer(self, text):
+		"""
+		Tokenization using spaCy
+
+		Parameters
+		----------
+		arg1 : list
+			A list of strings where each string is a single sentence
+
+		Returns
+		-------
+		list
+			A list of lists where each sub-list is a sequence of tokens
+		"""
+
+		tokenizedText = None
+
+		# Fill in code here
+
+		return tokenizedText
